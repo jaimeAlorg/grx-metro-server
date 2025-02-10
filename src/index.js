b61085a1d71c751115cfd74c26055372d6f36a89
jaimeAlorg <jaime.alorg1998@gmail.com>
2025-02-10 11:58:19 +0100

    fix: Now every time I fetch the data from the API, the websocket will send to the clients the updated data of the station they are in

--- src/index.js
+++ src/index.js
@@ -11,29 +11,51 @@ const app = express();
 app.use(cors());
 
 const interval = 60001;
+const wss = new WebSocketServer({ port: process.env.WS_PORT });
+const clients = new Map();
 
-transformTemplateToJSON();
-setInterval(transformTemplateToJSON, interval);
+updateAndSendTimes();
+setInterval(executeUpdateAndSend, interval);
 
 app.listen(process.env.PORT, () => {
    console.log(`Example app listening on port ${process.env.PORT}`);
 });
 
-//TODO hide port
-const wss = new WebSocketServer({ port: process.env.WS_PORT });
-
 wss.on('connection', (ws) => {
    ws.on('message', (message) => {
       const stationName = message.toString();
       const stationData = fetchStationByName(stationName);
-      buildArrivalTimesFromStation(stationName);
+
+      clients.set(ws, stationName);
 
       if (stationData) {
+         buildArrivalTimesFromStation(stationName);
          ws.send(JSON.stringify(stationData));
       }
    });
 
    ws.on('close', () => {
+      clients.delete(ws);
       console.log('Client disconnected');
    });
 });
+
+async function updateAndSendTimes() {
+   await transformTemplateToJSON();
+}
+
+async function executeUpdateAndSend() {
+   await updateAndSendTimes();
+   sendUpdatedTimes();
+}
+
+function sendUpdatedTimes() {
+   clients.forEach((stationName, ws) => {
+      const stationData = fetchStationByName(stationName);
+
+      if (stationData) {
+         buildArrivalTimesFromStation(stationName);
+         ws.send(JSON.stringify(stationData));
+      }
+   });
+}
